f9fc0af8e33b722e8abadd0b7fa10f34d5f4bdc6
Dominik Szczepek <theprogson@gmail.com>
2023-10-24 19:46:55 +0200

    python script working well

--- Python_merge_Audio_and_Video/merge.py
+++ Python_merge_Audio_and_Video/merge.py
@@ -6,20 +6,20 @@ import sys
 # main
 title = ""
 try:
-    title = sys.argv[1]
+    title = sys.argv[1].replace(sys.argv[0],"",1)
 except:
-    input("you need to give an arugment: -name_of_sketch")
-    exit()
+    title = input("you need to give a name of the sketch: -")
+
     
 # Open the video and audio
 try:
-    video_clip = VideoFileClip(f"../../Exports{title}V.mp4")
-    audio_clip = AudioFileClip(f"../../Exports{title}A.wav")
+    video_clip = VideoFileClip(f"../Exports/{title}V.mp4")
+    audio_clip = AudioFileClip(f"../Exports/{title}A.wav")
 except:
-    input(f"wrong name of file {title}")
+    print(f"wrong name of file {title}")
     exit()
 # Concatenate the video clip with the audio clip
 final_clip = video_clip.set_audio(audio_clip)
 
 # Export the final video with audio
-final_clip.write_videofile(f"../../Exports{title}.mp4")
+final_clip.write_videofile(f"../Exports/readyToUpload_{title}.mp4")
